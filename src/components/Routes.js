5249154a96f1129ad1f307c29cd5f6413fc86c46
ramialkaro <rami.alkaro@gmail.com>
2021-09-28 20:57:32 +0300

    add /places route and view for it.

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,14 +3,23 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
-} from "react-router-dom"
+} from 'react-router-dom'
+import {Box} from '@mui/material'
 import MapView from '../map'
+import PlacesView from '../places'
+import BottomNav from './BottomNav'
 
 export default function Routes(){
   return(
     <Router>
       <Switch>
-        <Route exact path="/" component={MapView} />
+        <Box sx={{pb:7}}>
+          <>
+          <Route exact path="/" component={MapView} />
+          <Route path="/places" component={PlacesView} />
+          </>
+        <BottomNav /> 
+        </Box>
       </Switch>
     </Router>
   )
