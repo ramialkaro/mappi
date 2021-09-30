bd8b77180321053dce89fd3fa74ac57e96993cba
ramialkaro <rami.alkaro@gmail.com>
2021-10-01 00:01:41 +0300

    message when empty list

---
+++ src/places/EmptyPlaces.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./emptyPlaces.css"
+
+export default function EmptyPlaces() {
+    return (
+      <div className="container">
+            <div className="empty-view">
+                    please navigate to <a href="/">Map View</a> to add more 
+            </div>
+      </div>
+    )
+}
