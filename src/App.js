937216cf7f8cb91fcaedfb2cda5bf73333ace7cb
ramialkaro <rami.alkaro@gmail.com>
2021-10-01 19:27:36 +0300

    use react context to handle the app state.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { PlacesContext } from './PlacesContext'
 import Routes from './components/Routes.js'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+  let storedPlaces = JSON.parse(localStorage.getItem("places")) || [] 
+  const [ listOfPlaces, setListOfPlaces ] = useState(storedPlaces)
   return (
-    <Routes />
+    <PlacesContext.Provider value={{ listOfPlaces, setListOfPlaces}}>
+      <Routes />
+    </PlacesContext.Provider>
   );
 }
 
